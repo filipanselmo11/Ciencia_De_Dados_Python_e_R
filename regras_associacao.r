5a02c1c73469da3e12f49e3525e7ba8e85ef2267
Filip Anselmo <filip_anselmo@yahoo.com.br>
2019-09-24 23:59:01 -0400

    adicionando regras de associacao com eclat

--- regras_associacao.r
+++ regras_associacao.r
@@ -11,3 +11,10 @@ regras = apriori(transacoes, parameter=list(supp=0.5, conf=0.5))
 inspect(regras)
 plot(regras)
 plot(regras, method="graph", control="")
+
+#Regras de asssociação com Eclat#
+
+regras = eclat(transacoes, parameter = list(supp=0.1, maxlen=15))
+library(arulesVizy)
+
+plot(regras, method="graph", control=list(type="items"))
